9da40971e890467803411121a43337214c4c1743
MichaelGrupp <mg91@arcor.de>
2015-02-15 23:15:15 +0100

    notifier example

--- appSync/synchronizedThreads.cpp
+++ appSync/synchronizedThreads.cpp
@@ -6,50 +6,41 @@
 #include <cstdlib> 
 #include <ctime> 
 
-void wait(int seconds)
-{
-	boost::this_thread::sleep(boost::posix_time::seconds(seconds));
-}
-
-boost::shared_mutex mutex;
-
-//this is the shared resource
+//create a mutex
+boost::mutex mutex;
+//this condition variable will be used for notifications of the threads
+boost::condition_variable_any cond;
+//shared resource
 std::vector<int> random_numbers;
 
-//fill() reads and writes to the shared resource --> use unique_lock
 void fill()
 {
 	std::srand(static_cast<unsigned int>(std::time(0)));
 	for (int i = 0; i < 3; ++i)
 	{
-		boost::unique_lock<boost::shared_mutex> lock(mutex);
+		//lock the mutex uniquely
+		boost::unique_lock<boost::mutex> lock(mutex);
 		random_numbers.push_back(std::rand());
-		lock.unlock();
-		wait(1);
+		//notify threads waiting for condition variable notification
+		cond.notify_all();
+		//wait for another thread to call cond.notify_all() and free mutex
+		cond.wait(mutex);
 	}
 }
 
-//print() reads only from the shared resource --> use shared_lock
 void print()
 {
+	std::size_t next_size = 1;
 	for (int i = 0; i < 3; ++i)
 	{
-		wait(1);
-		boost::shared_lock<boost::shared_mutex> lock(mutex);
-		std::cout << boost::this_thread::get_id() << "  " << random_numbers.back() << std::endl;
-	}
-}
-
-int sum = 0;
-
-//count() reads only from the shared resource --> use shared_lock
-void count()
-{
-	for (int i = 0; i < 3; ++i)
-	{
-		wait(1);
-		boost::shared_lock<boost::shared_mutex> lock(mutex);
-		sum += random_numbers.back();
+		//lock the mutex uniquely
+		boost::unique_lock<boost::mutex> lock(mutex);
+		while (random_numbers.size() != next_size)
+			cond.wait(mutex);
+		std::cout << random_numbers.back() << std::endl;
+		++next_size;
+		//notify waiting threads
+		cond.notify_all();
 	}
 }
 
@@ -57,11 +48,7 @@ int main()
 {
 	boost::thread t1(fill);
 	boost::thread t2(print);
-	boost::thread t3(count);
 	t1.join();
 	t2.join();
-	t3.join();
-	std::cout << "Summe: " << sum << std::endl;
-	
 	std::cin.get();
 }
