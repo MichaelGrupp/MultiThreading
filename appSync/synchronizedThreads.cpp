3ccd0f14e0d3e3f03944a013e808b03cf00a3216
MichaelGrupp <mg91@arcor.de>
2015-02-15 22:11:19 +0100

    finished mutex example

--- appSync/synchronizedThreads.cpp
+++ appSync/synchronizedThreads.cpp
@@ -8,6 +8,7 @@ void wait(int seconds)
 	boost::this_thread::sleep(boost::posix_time::seconds(seconds));
 }
 
+//create a mutex
 boost::mutex mutex;
 
 void thread()
@@ -15,6 +16,7 @@ void thread()
 	for (int i = 0; i < 5; ++i)
 	{
 		wait(1);
+		//if a mutex is locked, a section can only be used by a single thread that owns the mutex
 		mutex.lock();
 		std::cout << "Thread " << boost::this_thread::get_id() << ": " << i << std::endl;
 		mutex.unlock();
@@ -23,6 +25,7 @@ void thread()
 
 int main()
 {
+	//calling two same threads doesn't lead to to conflicts because 
 	boost::thread t1(thread);
 	boost::thread t2(thread);
 	t1.join();
